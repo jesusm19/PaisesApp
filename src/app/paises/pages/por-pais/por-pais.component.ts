695bf46b00153231039f99dc5e72b8f19904387a
Jesus Garcia <jesus_heavy19@hotmail.com>
2023-03-30 12:15:33 -0600

    autocomplete de sugerencias

--- src/app/paises/pages/por-pais/por-pais.component.ts
+++ src/app/paises/pages/por-pais/por-pais.component.ts
@@ -4,11 +4,19 @@ import { Country } from '../../interfaces/paises.interface';
 
 @Component({
   selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html'
+  templateUrl: './por-pais.component.html',
+  styles:[
+  `
+    li {
+      cursor: pointer;
+    }
+  `
+  ]
 })
 export class PorPaisComponent {
 
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
   pais: string = '';
   isError: boolean = false;
   placeholder: string ='Buscar por país...';
@@ -36,6 +44,16 @@ export class PorPaisComponent {
 
   buscarSugerencias(sugerencia: string ){
     this.isError = false;
+    this.pais = sugerencia;
+    this.paisesService.buscarPais(sugerencia).subscribe({
+      next: paises => {
+        this.paisesSugeridos = paises.splice(0,5);
+      },
+      error: error => {
+        console.error(error);
+        this.paisesSugeridos = [];
+      }
+    });
   }
 
 }
